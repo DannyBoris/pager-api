dfb42ae54b887662e27ef129540f3ad3bb4a762a
Danny Borisov <dannybo@waves.com>
2023-04-11 01:09:47 +0700

    fixed error

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const fs = require("fs");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
 const main_routes = require("./routes");
-const https = require("https");
-const options = {
-  key: fs.readFileSync(path.resolve(__dirname, "./private.key")),
-  cert: fs.readFileSync(path.resolve(__dirname, "./certificate.crt")),
-};
-
-
 
 app.use(express.static("./frontend/dist"));
 
@@ -20,4 +11,6 @@ app.use(cors());
 app.use(bodyParser({ extended: true }));
 app.use(main_routes);
 console.log(process.env.PORT);
-app.listen(8080)
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on ${process.env.PORT}`)
+);
